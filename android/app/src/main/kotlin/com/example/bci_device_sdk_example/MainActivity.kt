af2a38aac85e60be19d82c968947b6c1d095f3f2
alexcode9 <alex.apr9@gmail.com>
2025-03-18 14:18:25 +0100

    recreated windows and android folders with newer flutter

---
+++ android/app/src/main/kotlin/com/example/bci_device_sdk_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bci_device_sdk_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/tech/brainco/bci_device_sdk_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package tech.brainco.bci_device_sdk_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
